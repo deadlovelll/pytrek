ba5a0f453fc394c77fb66b5b97eb5d1a862be457
Timofey Ivankov <playboicartina@gmail.com>
2025-11-09 00:44:39 +0300

    added ast parser

--- src/core/project_intializer/ast_parser.rs
+++ src/core/project_intializer/ast_parser.rs
@@ -1,4 +1,8 @@
-use python_ast::parse;
+use std::{fs, io::Read};
+use std::path::Path;
+
+use tree_sitter::{Parser, TreeCursor};
+use tree_sitter_python;
 
 pub struct AstParser {}
 
@@ -7,7 +11,13 @@ impl AstParser {
         Self {}
     }
 
-    pub fn parse(&self) {
-        print!("parsing tree");
+    pub fn parse(&self, path: &Path) {
+        let code = fs::read_to_string(path).expect("Failed to read file");
+        let mut parser = tree_sitter::Parser::new();
+        let language = tree_sitter_python::LANGUAGE;
+        parser
+            .set_language(&language.into())
+            .expect("Error loading Python parser");
+        let tree = parser.parse(code, None).unwrap();
     }
 }
--- src/core/project_intializer/graph_creator.rs
+++ src/core/project_intializer/graph_creator.rs
@@ -1,16 +1,19 @@
 use std::path::Path;
 use std::collections::HashMap;
 
+use crate::core::project_intializer::ast_parser::AstParser;
+
 pub struct GraphCreator {
-    ast_data: HashMap<String, String>
+    ast_data: HashMap<String, String>,
+    ast_parser: AstParser,
 }
 
 impl GraphCreator {
     pub fn new() -> Self {
-        Self {ast_data: HashMap::new()}
+        Self {ast_data: HashMap::new(), ast_parser: AstParser::new()}
     }
 
     pub fn create_graph(&self, path: &Path) {
-        print!("hello");
+        self.ast_parser.parse(path);
     }
 }
