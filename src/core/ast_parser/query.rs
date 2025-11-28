21c2036cc816757c2972e82b61a5c3a1efee5753
Timofey Ivankov <playboicartina@gmail.com>
2025-11-28 18:44:58 +0300

    more tests fo query

--- src/core/ast_parser/query.rs
+++ src/core/ast_parser/query.rs
@@ -29,7 +29,6 @@ mod tests {
     use std::collections::HashSet;
 
     use super::*;
-    use blake3::Hash;
     use tree_sitter::QueryCursor;
     use tree_sitter::StreamingIterator;
 
@@ -42,7 +41,7 @@ mod tests {
     }
 
     #[test]
-    fn test_query_returns_imports() {
+    fn test_query_returns_imports_mix() {
         let code = r#"
             import os
             from sys import path
@@ -74,9 +73,97 @@ mod tests {
         while let Some(m) = matches.next() {
             for cap in m.captures.iter() {
                 let name = results.capture_names()[cap.index as usize];
+                println!("{}", name);
                 seen.insert(name);
             }
         }
-        assert_eq!(seen.len(), 3);
+        assert_eq!(seen.contains("import"), true);
+        assert_eq!(seen.contains("module"), true);
+        assert_eq!(seen.contains("name"), true);
+
+    }
+
+    #[test]
+    fn test_query_returns_imports() {
+        let code = r#"
+            import os
+            import sys
+            import fastapi
+            import settings
+        "#;
+
+        let query = ParseQuery::new();
+        let language: Language = tree_sitter_python::LANGUAGE.into();
+
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&language)
+            .expect("Error loading Python parser");
+        
+        let tree = parser.parse(&code, None).unwrap();
+        let root_node = tree.root_node();
+        let results = query.get(language);
+        
+        let mut cursor = QueryCursor::new();
+        let mut matches = cursor.matches(
+            &results, 
+            root_node, 
+            code.as_bytes()
+        );
+
+        let mut seen: HashSet<&str> = HashSet::new();
+        
+        while let Some(m) = matches.next() {
+            for cap in m.captures.iter() {
+                let name = results.capture_names()[cap.index as usize];
+                println!("{}", name);
+                seen.insert(name);
+            }
+        }
+        assert_eq!(seen.contains("import"), true);
+        assert_eq!(seen.contains("module"), false);
+        assert_eq!(seen.contains("name"), false);
+    }
+
+    #[test]
+    fn test_query_returns_imports_from() {
+        let code = r#"
+            from os import osos
+            from sys import syscall
+            from fastapi import ApiRouter
+            from settings import KafkaDsn
+        "#;
+
+        let query = ParseQuery::new();
+        let language: Language = tree_sitter_python::LANGUAGE.into();
+
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&language)
+            .expect("Error loading Python parser");
+        
+        let tree = parser.parse(&code, None).unwrap();
+        let root_node = tree.root_node();
+        let results = query.get(language);
+        
+        let mut cursor = QueryCursor::new();
+        let mut matches = cursor.matches(
+            &results, 
+            root_node, 
+            code.as_bytes()
+        );
+
+        let mut seen: HashSet<&str> = HashSet::new();
+        
+        while let Some(m) = matches.next() {
+            for cap in m.captures.iter() {
+                let name = results.capture_names()[cap.index as usize];
+                println!("{}", name);
+                seen.insert(name);
+            }
+        }
+        assert_eq!(seen.contains("import"), false);
+        assert_eq!(seen.contains("module"), true);
+        assert_eq!(seen.contains("name"), true);
     }
 }
